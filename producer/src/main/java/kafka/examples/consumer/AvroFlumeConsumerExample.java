e0a6a9abe1835757c5d48634d8f48e0472d947b9
kwon young <kykim@mobigen.com>
2020-05-03 04:54:55 +0900

    commit

--- producer/src/main/java/kafka/examples/consumer/AvroFlumeConsumerExample.java
+++ producer/src/main/java/kafka/examples/consumer/AvroFlumeConsumerExample.java
@@ -18,8 +18,6 @@
 
 package kafka.examples.consumer;
 
-import com.google.common.collect.Lists;
-import kafka.examples.producer.Util;
 import net.sourceforge.argparse4j.ArgumentParsers;
 import net.sourceforge.argparse4j.inf.ArgumentParser;
 import net.sourceforge.argparse4j.inf.ArgumentParserException;
@@ -29,13 +27,12 @@ import org.apache.avro.io.DecoderFactory;
 import org.apache.avro.specific.SpecificDatumReader;
 import org.apache.avro.util.Utf8;
 import org.apache.commons.lang.SerializationException;
+import org.apache.commons.lang.StringUtils;
 import org.apache.flume.source.avro.AvroFlumeEvent;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.common.PartitionInfo;
-import org.apache.kafka.common.TopicPartition;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -45,9 +42,6 @@ import java.util.*;
 import static net.sourceforge.argparse4j.impl.Arguments.store;
 
 public class AvroFlumeConsumerExample {
-
-
-
     public static void main(String[] args) {
         ArgumentParser parser = argParser();
 
@@ -57,39 +51,24 @@ public class AvroFlumeConsumerExample {
             /* parse args */
             String brokerList = res.getString("bootstrap.servers");
             String topic = res.getString("topic");
-            int epoch_column = 1;
-            try {
-                epoch_column = res.getInt("epoch_column");
-            } catch (Exception e) {
-                System.out.println("epoch_column option is not used. default value is 1");
-            }
-
             String serializer = res.getString("serializer");
 
             String groupId = res.getString("group.id");
+            String inputDelimiter = res.getString("id");
+            String outputDelimiter = res.getString("od");
 
             final SimpleDateFormat df = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 
-            final String DEFAULT_SEPARATOR = "\036";
 
             Properties consumerConfig = new Properties();
-            consumerConfig.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
-            consumerConfig.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,brokerList);
-//            consumerConfig.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
-            consumerConfig.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
+            consumerConfig.put("group.id", groupId);
+            consumerConfig.put("bootstrap.servers",brokerList);
+            consumerConfig.put("auto.offset.reset", "latest");
             consumerConfig.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.ByteArrayDeserializer");
             consumerConfig.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
 
             KafkaConsumer<Object, Object> consumer = new KafkaConsumer<>(consumerConfig);
             consumer.subscribe(Collections.singletonList(topic));
-//            Map<String, List<PartitionInfo>> topicInfos = consumer.listTopics();
-//            List<PartitionInfo> partitions = topicInfos.get(topic);
-//            for(PartitionInfo partition : partitions) {
-//                TopicPartition topicPartition = new TopicPartition(topic, partition.partition());
-//                List<TopicPartition> topicPartitions = Lists.newArrayList(topicPartition);
-//                consumer.assign(topicPartitions);
-//                consumer.seekToEnd(topicPartitions);
-//            }
 
             while (true) {
                 ConsumerRecords<Object, Object> records = consumer.poll(1000);
@@ -101,30 +80,23 @@ public class AvroFlumeConsumerExample {
                         Map<CharSequence, CharSequence> headers = event.getHeaders();
 
                         StringBuilder sb = new StringBuilder();
-                        headers.entrySet().forEach(entry -> {
-                            sb.append(entry.getKey()).append(": ");
-                            if(entry.getKey().equals(new Utf8("timestamp"))) {
-                                sb.append(df.format(new Date(Long.parseLong(entry.getValue().toString())))).append(", ");
+                        headers.forEach((key, value) -> {
+                            sb.append(key).append(": ");
+                            if (key.equals(new Utf8("timestamp"))) {
+                                sb.append(df.format(new Date(Long.parseLong(value.toString())))).append(", ");
                             } else {
-                                sb.append(entry.getValue()).append(", ");
+                                sb.append(value).append(", ");
                             }
                         });
-//                        sb.append(StandardCharsets.UTF_8.decode(event.getBody()));
 
-                        String body = String.format("%s", StandardCharsets.UTF_8.decode(event.getBody()));
-                        sb.append(body);
-
-                        String[] epoch =  Util.getNthColumn(body, epoch_column).split("\\.");
-                        try {
-                            String logTime = df.format(new Date(Long.parseLong(epoch[0] + epoch[1].substring(0, 3))));
-                            sb.append(String.format(" log-time:%s", logTime));
-                            System.out.println(String.format("%s",sb.toString()));
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                            for (String s : epoch) {
-                                System.out.println("epoch index exception: " + s);
-                            }
+                        if(inputDelimiter == null) {
+                            sb.append("body: ").append(StandardCharsets.UTF_8.decode(event.getBody()));
+                        } else {
+                            String[] cols = String.valueOf(StandardCharsets.UTF_8.decode(event.getBody())).split(inputDelimiter);
+                            sb.append("body: ").append(StringUtils.join(cols, outputDelimiter == null ? " | " : outputDelimiter));
                         }
+
+                        System.out.println(sb.toString());
                     }
                 } catch (SerializationException e) {
                     System.out.printf("exception: %s\n", e.getMessage());
@@ -193,12 +165,17 @@ public class AvroFlumeConsumerExample {
                 .metavar("BYTE/KRYO")
                 .help("use byte array or kryo serializer");
 
-        parser.addArgument("--epoch_column").action(store())
-              .required(false)
-              .setDefault(1)
-              .type(Integer.class)
-              .metavar("INTEGER")
-              .help("epoch column index to convert to timestamp");
+        parser.addArgument("--id").action(store())
+                .required(false)
+                .type(String.class)
+                .metavar("STRING")
+                .help("input delimiter");
+
+        parser.addArgument("--od").action(store())
+                .required(false)
+                .type(String.class)
+                .metavar("STRING")
+                .help("output delimiter");
 
         return parser;
     }
