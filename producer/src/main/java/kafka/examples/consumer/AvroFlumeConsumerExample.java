cc250fecec22c052218ba87bbbc04b8a0fb46bb0
kwon young <kykim79@gmail.com>
2020-07-29 15:38:11 +0900

    refactored

--- producer/src/main/java/kafka/examples/consumer/AvroFlumeConsumerExample.java
+++ producer/src/main/java/kafka/examples/consumer/AvroFlumeConsumerExample.java
@@ -39,6 +39,7 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.text.SimpleDateFormat;
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
 
 import static net.sourceforge.argparse4j.impl.Arguments.store;
@@ -60,6 +61,9 @@ public class AvroFlumeConsumerExample {
             String outputDelimiter = res.getString("od");
             Boolean printOutput = res.getBoolean("output");
             Boolean countEvent = res.getBoolean("count");
+            Integer requiredCols = res.getInt("cols");
+
+            String autoOffsetReset = res.getString("auto.offset.reset");
 
             final SimpleDateFormat df = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 
@@ -69,39 +73,62 @@ public class AvroFlumeConsumerExample {
             Properties consumerConfig = new Properties();
             consumerConfig.put("group.id", groupId);
             consumerConfig.put("bootstrap.servers",brokerList);
-            consumerConfig.put("auto.offset.reset", "latest");
+            consumerConfig.put("auto.offset.reset", autoOffsetReset != null ? autoOffsetReset : "latest");
+            consumerConfig.put("session.timeout.ms", 30000);
+            consumerConfig.put("max.poll.records", 10000);
+
             consumerConfig.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.ByteArrayDeserializer");
             consumerConfig.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
 
             KafkaConsumer<Object, Object> consumer = new KafkaConsumer<>(consumerConfig);
             consumer.subscribe(Collections.singletonList(topic));
-            Map<String, Long> minCounter = new HashMap<>();
+            Map<String, Long> counter = new ConcurrentHashMap<>();
 
             if(countEvent) {
+                Calendar startCal = Calendar.getInstance();
+                startCal.set(Calendar.SECOND, 0);
+
                 Calendar cal = Calendar.getInstance();
                 Timer printTimer = new Timer();
                 printTimer.schedule(new TimerTask() {
                     @Override
                     public void run() {
                         cal.setTime(new Date());
-                        cal.add(Calendar.MINUTE, -1);
+                        cal.add(Calendar.MINUTE, -2);
                         String date = df_min.format(cal.getTime());
-                        minCounter.keySet().stream().filter(key -> key.startsWith(date))
-                                  .map(key -> String.format("[Message Count] %s:%d", key, minCounter.get(key)))
+                        counter.keySet().stream().filter(key -> key.startsWith(date))
+                                  .map(key -> String.format("[Message Count] %s:%d", key, counter.get(key)))
                                   .forEach(System.out::println);
                     }
-                }, 0, TimeUnit.MINUTES.toMillis(1));
+                }, startCal.getTime(), TimeUnit.MINUTES.toMillis(1));
+
+                Calendar startCal2 = Calendar.getInstance();
+                startCal2.set(Calendar.MINUTE, 5);
+                startCal2.set(Calendar.SECOND, 0);
+
+                Timer printTimer2 = new Timer();
+                printTimer2.schedule(new TimerTask() {
+                    @Override
+                    public void run() {
+                        cal.setTime(new Date());
+                        cal.add(Calendar.HOUR, -2);
+                        String date = df_hour.format(cal.getTime());
+                        counter.keySet().stream().filter(key -> key.startsWith(date))
+                                  .map(key -> String.format("[Message Count] %s:%d", key, counter.get(key)))
+                                  .forEach(System.out::println);
+                    }
+                }, startCal.getTime(), TimeUnit.HOURS.toMillis(1));
 
                 Timer removeTimer = new Timer();
                 removeTimer.schedule(new TimerTask() {
                     @Override
                     public void run() {
                         cal.setTime(new Date());
-                        cal.add(Calendar.HOUR, -1);
+                        cal.add(Calendar.HOUR, -3);
                         String date = df_hour.format(cal.getTime());
-                        minCounter.keySet().removeIf(key -> key.startsWith(date));
+                        counter.keySet().removeIf(key -> key.startsWith(date));
                     }
-                }, 0, TimeUnit.HOURS.toMillis(1));
+                }, startCal2.getTime(), TimeUnit.HOURS.toMillis(1));
             }
 
             while (true) {
@@ -113,7 +140,7 @@ public class AvroFlumeConsumerExample {
 
                         Map<CharSequence, CharSequence> headers = event.getHeaders();
                         if(countEvent) {
-                            count(headers, record, minCounter);
+                            count(headers, record, counter);
                         }
 
                         StringBuilder sb = new StringBuilder();
@@ -131,7 +158,10 @@ public class AvroFlumeConsumerExample {
                             sb.append("body: ").append(StandardCharsets.UTF_8.decode(event.getBody()));
                         } else {
                             String[] cols = String.valueOf(StandardCharsets.UTF_8.decode(event.getBody())).split(inputDelimiter);
-                            sb.append("body: ").append(StringUtils.join(cols, outputDelimiter == null ? " | " : outputDelimiter));
+                            if(requiredCols != null && requiredCols > cols.length) {
+                                System.out.println(String.format("[%s] invalid column size. required: %d, actual: %d", df.format(new Date()), requiredCols, cols.length));
+                            }
+                            sb.append("body: ").append(StringUtils.join(cols, outputDelimiter == null ? inputDelimiter : outputDelimiter));
                         }
 
                         if(printOutput) {
@@ -155,13 +185,34 @@ public class AvroFlumeConsumerExample {
         }
     }
 
-    private static void count(Map<CharSequence, CharSequence> headers, ConsumerRecord<Object, Object> record, Map<String, Long> minCounter) {
-        final String dt = headers.get(new Utf8("dt")).toString();
-        final String hh = headers.get(new Utf8("hh")).toString();
-        final String mm = headers.get(new Utf8("mm")).toString();
-        final String nw = headers.get(new Utf8("nw")).toString();
-        final String key = String.format("%s%s%s_%s_%s", dt, hh, mm, nw, record.topic());
-        minCounter.put(key, minCounter.containsKey(key) ? minCounter.get(key) + 1 : 1);
+    private static void count(Map<CharSequence, CharSequence> headers, ConsumerRecord<Object, Object> record, Map<String, Long> counter) {
+        String dt = "";
+        if(headers.containsKey(new Utf8("dt"))) {
+            dt = headers.get(new Utf8("dt")).toString();
+        }
+
+        String hh = "";
+        if(headers.containsKey(new Utf8("hh"))) {
+            hh = headers.get(new Utf8("hh")).toString();
+        }
+
+        String mm = "";
+        if(headers.containsKey(new Utf8("mm"))){
+            mm = headers.get(new Utf8("mm")).toString();
+        }
+
+        String nw = "";
+        if(headers.containsKey(new Utf8("nw"))) {
+            nw = headers.get(new Utf8("nw")).toString();
+        }
+
+        String sys = "";
+        if(headers.containsKey(new Utf8("sys"))) {
+            sys = headers.get(new Utf8("sys")).toString();
+        }
+
+        final String key = String.format("%s%s%s_%s_%s_%s", dt, hh, mm, nw, sys, record.topic());
+        counter.put(key, counter.containsKey(key) ? counter.get(key) + 1 : 1);
     }
 
     private static AvroFlumeEvent deserialize(byte[] bytes)  {
@@ -236,6 +287,18 @@ public class AvroFlumeConsumerExample {
               .setDefault(false)
               .type(Arguments.booleanType("yes", "no"))
               .help("count message(min)");
+
+        parser.addArgument("--cols").action(store())
+              .required(false)
+              .type(Integer.class)
+              .metavar("REQUIRED-COLS")
+              .help("required columns");
+
+        parser.addArgument("--auto.offset.reset").action(store())
+              .required(false)
+              .type(String.class)
+              .metavar("STRING")
+              .help("auto.offset.reset");
 
         return parser;
     }
